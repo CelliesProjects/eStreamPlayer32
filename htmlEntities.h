a9642e2a0df00080ac3016b59fbb57ded11a5968
Cellie <CelliesProjects@users.noreply.github.com>
2020-10-24 11:22:32 +0200

    Fixes and comment

--- htmlEntities.h
+++ htmlEntities.h
@@ -4,10 +4,10 @@ String htmlEntities(const char* plaintext) {
   String result{};
   uint32_t cnt{0};
   while (plaintext[cnt] != 0) {
-    if (plaintext[cnt] >= 0xA0) {
+    if (plaintext[cnt] > 128) {
       switch (plaintext[cnt]) {
 
-        case 0xC2 :                                 //UTF-8 16bit encoding - just copy
+        case 0xC2 :                                 //UTF-8 16bit encoding - just copy 2 bytes to result
         case 0xC3 :
           result.concat(plaintext[cnt]);
           cnt++;
