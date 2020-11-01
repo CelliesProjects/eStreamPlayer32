437565429e2f59d42e3ff6e9b10f17f6e715db23
Cellie <CelliesProjects@users.noreply.github.com>
2020-11-01 20:02:10 +0100

    CSS.

--- copy to server/eSP32.php
+++ copy to server/eSP32.php
@@ -3,7 +3,7 @@
   const emptyicon='<img src="/emptyicon.svg" class="foldericon">';
   const folderup='<svg class="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M11 9l1.42 1.42L8.83 14H18V4h2v12H8.83l3.59 3.58L11 21l-6-6 6-6z"/></svg>';
   const audioicon='<svg class="icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 3v9.28c-.47-.17-.97-.28-1.5-.28C8.01 12 6 14.01 6 16.5S8.01 21 10.5 21c2.31 0 4.2-1.75 4.45-4H15V6h4V3h-7z"/></svg>';
-  const starticon='<img src="/starticon.svg" class="icon">';
+  const starticon='<img src="/starticon.svg" class="icon starticon">';
 header('Access-Control-Allow-Origin: *');
 
 if(isset($_GET["folder"])){
@@ -36,11 +36,11 @@ if(isset($_GET["folder"])){
       echo emptyicon;
       echo emptyicon;
     }
-    echo $pieces[count($pieces)-1].'</div>';
+    echo '<span class="text">'.$pieces[count($pieces)-1].'</span></div>';
   }
   foreach(glob($path.$validFiles,GLOB_BRACE)as$filename){
     $pieces=explode('/',$filename);
-    echo '<div class="filelink">'.audioicon.starticon.$pieces[count($pieces)-1].'</div>';
+    echo '<div class="filelink">'.audioicon.starticon.'<span class="text">'.$pieces[count($pieces)-1].'</span></div>';
   }
   die();
 }
