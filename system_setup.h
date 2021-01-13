bbf54a70e26c3353c2958f9b463ec0e90681deab
Cellie <CelliesProjects@users.noreply.github.com>
2021-01-13 13:29:54 +0100

    Added SCRIPT_URL to system setup.

--- system_setup.h
+++ system_setup.h
@@ -8,6 +8,9 @@ const char * PSK = "xxx";
 //#define   A1S_AUDIO_KIT
 //#define   M5STACK_NODE
 
+/* SCRIPT_URL should point to the php script on the music file server */
+const char * SCRIPT_URL = "http://192.168.0.30/music/eSP32.php";
+
 /* If SET_STATIC_IP is set to true then STATIC_IP, GATEWAY, SUBNET and PRIMARY_DNS have to be set to some sane values */
 const bool SET_STATIC_IP = false;
 
