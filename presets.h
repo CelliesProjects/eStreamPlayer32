7aa2de848f9993fe62cb60cd103141e3a4c1c371
Cellie <CelliesProjects@users.noreply.github.com>
2021-03-13 09:31:59 +0100

    Update presets.

--- presets.h
+++ presets.h
@@ -15,11 +15,11 @@ const source preset[] {
   {"NPO Radio5", "http://icecast.omroep.nl/radio5-bb-mp3"},
   {"538 Dance Department", "http://22193.live.streamtheworld.com/TLPSTR01.mp3"},
   {"Radio 10 Disco Classics", "http://19993.live.streamtheworld.com/RADIO10.mp3"},
+  {"XXL Stenders", "http://streams.robstenders.nl:8063/bonanza_mp3"},
   {"RadioEins", "http://rbb-edge-1073-dus-dtag-cdn.cast.addradio.de/rbb/radioeins/live/mp3/128/stream.mp3"},
   {"BBC Radio 1", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"},
   {"BBC Radio 2", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p"},
   {"BBC Radio 3", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p"},
   {"BBC Radio 4FM", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p"},
   {"BBC Radio 6 Music", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_6music_mf_p"},
-  {"Amsterdams Most Wanted", "http://piratenstreams.nl:8000"},
 };
